05ca174887f121e89a753fb3d3309cd169f31deb
sqbr <45616686+spiralatlas@users.noreply.github.com>
2023-05-05 13:23:21 +0800

    tweaked shoes

--- code/generate_images.py
+++ code/generate_images.py
@@ -839,7 +839,7 @@ def runStuff():
     # "coat","coat_sleeves","coat_dec","coat_back"
     #"top","top_sleeves","top_dec","top_collar"
     for c in closet:
-        if c.name in ["ears"]:
+        if c.name in ["shoes"]:
             process_portrait_part(c)
     makeWinks()
     #makeStubble() 
