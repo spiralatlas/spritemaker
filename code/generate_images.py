e54f1232c282a9acde82cad43d1d28491899c77d
sqbr <45616686+spiralatlas@users.noreply.github.com>
2023-05-25 15:16:50 +0800

    fixing more shading

--- code/generate_images.py
+++ code/generate_images.py
@@ -881,7 +881,7 @@ def runStuff():
     # "body_chest","top_chest","overshirt_chest","coat_chest"
     # "neckwear","neckwear_dec"
     for c in closet:
-        if c.name in ["overshirt","overshirt_dec"]:
+        if c.name in ["neckwear"]:
             process_portrait_part(c)
     makeWinks()
     #makeStubble() 
