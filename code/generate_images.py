8493db73abfb6a88d65d50419e1ffbd7485febea
sqbr <45616686+spiralatlas@users.noreply.github.com>
2023-05-26 22:08:32 +0800

    skull

--- code/generate_images.py
+++ code/generate_images.py
@@ -881,7 +881,7 @@ def runStuff():
     # "body_chest","top_chest","overshirt_chest","coat_chest"
     # "neckwear","neckwear_dec"
     for c in closet:
-        if c.name in [ "facial_hair" ]:
+        if c.name in [ "skull" ]:
             process_portrait_part(c)
     makeWinks()
     #makeStubble() 
