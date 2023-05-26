cdfba961e04e714f7793a0a93a5b264b231bbd2b
sqbr <45616686+spiralatlas@users.noreply.github.com>
2023-05-26 15:13:59 +0800

    hair extra

--- code/generate_images.py
+++ code/generate_images.py
@@ -881,7 +881,7 @@ def runStuff():
     # "body_chest","top_chest","overshirt_chest","coat_chest"
     # "neckwear","neckwear_dec"
     for c in closet:
-        if c.name in [ "body_chest","top_chest","overshirt_chest","coat_chest"]:
+        if c.name in [ "sidelocks", "fringe", "hair_front","hair_middle", "hair_back","hair_extra", ]:
             process_portrait_part(c)
     makeWinks()
     #makeStubble() 
