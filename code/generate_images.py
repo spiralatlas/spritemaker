da9c4875c1bcd2e465cad85ae01b3a7438ad648c
sqbr <45616686+spiralatlas@users.noreply.github.com>
2023-05-28 13:14:28 +0800

    chest

--- code/generate_images.py
+++ code/generate_images.py
@@ -869,7 +869,7 @@ def runStuff():
     #"earrings", "earrings_dec"
     # "hat_back","hat_middle","hat_front"
     for c in closet:
-        if c.name in [ "skull", "head","body","legs", ]:
+        if c.name in ["ears","nose", "chest" ]:
             process_portrait_part(c)
     makeWinks()
     #makeStubble() 
