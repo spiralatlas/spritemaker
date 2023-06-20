2d4ac9153ad4ce4cfba5778eb12e6feaf0b587c2
sqbr <45616686+spiralatlas@users.noreply.github.com>
2023-06-20 20:56:09 +0800

    human readable summary finished

--- code/menus.js
+++ code/menus.js
@@ -6,29 +6,38 @@ function human_readable_object(obj){
     let output = ""
     for (let i = 0; i < Object.keys(obj).length; i += 1){
         current_varname = Object.keys(obj)[i];
-        output+="//"+current_varname+": ";
         if (current_varname.length >8 && current_varname.slice(0,8)=="current_"){//&& Number.isInteger(obj[current_varname])){
-            output+=human_readable_variable(varList(current_varname),obj[current_varname])+"\n";
+            output+="//"+current_varname+": "+human_readable_variable(varList(current_varname),obj[current_varname])+"\n";
         }
         else{
-            current_value = obj[current_varname];
-            switch (current_varname){
-                case "pattern":
-                    output+=pattern_list[current_value]+"\n";
-                    break;
-                case "value_list":
-                    objList = defining_objects[findDefiningIndex(obj.name)].item_list;
-                    if (expression_list.includes(obj.name))
-                        output+=current_value+"\n";
-                    else    
-                        output+=objList[current_value[0]]+"\n";
-                    break;    
-                default:    
-                    output+=current_value+"\n";
-                    break;
-            }
+            has_colours = true;
+            colours_children_list = skin_list_defining.concat(['cheeks', 'hair_extra','fringe', 'facial_hair', 'waistline','sidelocks'])
+            colours_children_list= colours_children_list.concat(has_sleeves_list.map(value => value+"_sleeves"))
+            if (colours_children_list.includes(obj.name))
+                has_colours = false;
+            if (has_colours || !["pattern","colour1","colour2","patterncolour"].includes(current_varname)){    
+                current_value = obj[current_varname];
+                switch (current_varname){
+                    case "pattern":
+                        output+="//"+current_varname+": "+pattern_list[current_value]+"\n";
+                        break;
+                    case "value_list":
+                        objList = defining_objects[findDefiningIndex(obj.name)].item_list;
+                        if (expression_list.includes(obj.name)){
+                            output+="//value_list: "
+                            for (let j = 0; j < current_value.length; j += 1)
+                                output+=objList[current_value[j]]+", "
+                            output+="\n";
+                        }
+                        else    
+                            output+="//value: "+objList[current_value[0]]+"\n";
+                        break;    
+                    default:    
+                        output+="//"+current_varname+": "+current_value+"\n";
+                        break;
+                }
                 
-    }}
+    }}}
     output+="\n";
     return output
 }
@@ -50,19 +59,19 @@ function human_readable(vars){
         obj_name = Object.keys(vars)[i];
         switch(obj_name){
             case "current_defining_objects":
-                output+="//Body and Outfit:\n";
+                output+="//* Body and Outfit *\n\n";
                 output+=human_readable_list(vars["current_defining_objects"])
                 break;
             case "ui_variables_object":
-                output+="//User interface:\n";
+                output+="//* User interface *\n\n";
                 output+=human_readable_object(vars["ui_variables_object"])
                 break; 
             case "defining_variables_object":
-                output+="//Other:\n";
+                output+="//* Other *\n\n";
                 output+=human_readable_object(vars["defining_variables_object"])
                 break;           
         }
-        output+="\n"
+        //output+="\n"
     }
     output+="// *** END SUMMARY ***\n\n"
     return output
